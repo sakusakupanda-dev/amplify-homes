8d2e60ecd3f2c4e4165ec0e140e24e987675b88f
sakusakupanda-dev <digital04@ryuseki.co.jp>
2023-06-09 12:31:00 +0900

    new file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+// 新しいページのコンポーネントをインポート
+import Page2 from './Page2';
+
+Amplify.configure({
+  aws_project_region: process.env.REACT_APP_AWS_PROJECT_REGION,
+  aws_cognito_region: process.env.REACT_APP_AWS_COGNITO_REGION,
+  aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
+  aws_user_pools_web_client_id: process.env.REACT_APP_AWS_USER_POOLS_CLIENT_ID,
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+      <Route exact path="/" component={Home} />
+          {({ history }): { history: any })  => (
+            <Authenticator hideSignUp={true} loginMechanisms={['email']}>
+              {({ signOut, user }) => (
+                <main>
+                  <h1>Hello</h1>
+                  <button onClick={signOut}>Sign out</button>
+                  <button onClick={() => { history.push('/page2') }}>Go to Page 2</button>
+                </main>
+              )}
+            </Authenticator>
+          )}
+        </Route>
+        <Route exact path="/page2" component={Page2} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Page2.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Page2() {
+  return (
+    <main>
+      <h1>This is Page 2</h1>
+    </main>
+  );
+}
+
+export default Page2;
